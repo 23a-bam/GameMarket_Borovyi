9bd50ae529ce6d41c680c94cf98171835d8b2872
23a-bam <23a_bam@liceum.ztu.edu.ua>
2024-05-15 08:44:06 +0300

    new code and items

--- market.js
+++ market.js
@@ -2,6 +2,17 @@ console.log("market.js script connected.");
 
 let itemsDiv = document.getElementById("items");
 
+let itemsArray = [
+    'Газонокосарка 41',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумуляторний оприскувач 12',
+    'Акумулятор',
+    'Генератор',
+    'Телевізор',
+    'Ліхтарик',
+];
+
 // Перевірка існування знайденого блоку
 if (itemsDiv) {
     // Вивід знайденого елементу
@@ -27,15 +38,29 @@ if (itemsDiv) {
     */
 
     // Додавання нових item в блок
+    /*
     itemsDiv.innerHTML += '<div class="item">Генератор 1</div>';
     itemsDiv.innerHTML += '<div class="item">Генератор 2</div>';
     itemsDiv.innerHTML += '<div class="item">Генератор 3</div>';
     itemsDiv.innerHTML += '<div class="item">Генератор 4</div>';
-
+    */
     // Додавання ще 96 товарів
+    /*
     for (let i = 1; i <= 96; i++) {
         itemsDiv.innerHTML += '<div class="item">Телевізор ' + i + '</div>';
     }
+    */
+    // console.log(itemsArray);
+
+    itemsArray.forEach((item, index) => {
+        itemsDiv.innerHTML +=
+        `
+        <div class = "item">
+            <h2>Товар №${index + 1} з ${itemsArray.length}</h2>
+            <p>${item}</p>
+        </div>
+        `
+    });
 } else {
     // Вивід повідомлення про не знайдений блок
     console.log("items not found!");
